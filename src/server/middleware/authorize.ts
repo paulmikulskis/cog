6fb215e5802be50b293656020fd9687a19122733
dreivan <dreivan.orprecio@gmail.com>
2022-09-21 20:42:21 +0800

    Initial Draft

---
+++ src/server/middleware/authorize.ts
@@ -0,0 +1,44 @@
+import { Request, Response, NextFunction } from "express";
+import { respondWith } from "../utils/server_utils";
+import { app } from "firebase-admin/lib/firebase-namespace-api";
+
+const authToken = (firebaseAdmin: app.App | undefined) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    const authHeader = req.headers["authorization"]?.split(" ");
+    const admin = authHeader && authHeader[0];
+
+    if (admin === "admin") {
+      /* 
+      Logic 1 goes here
+
+
+      */
+      return next();
+    }
+    const token = authHeader && authHeader[1];
+
+    if (!token) {
+      return res.send(respondWith(403, `Invalid Token`));
+    }
+
+    if (!firebaseAdmin)
+      return res.send(respondWith(403, `Authentication Server Error`));
+
+    firebaseAdmin
+      .auth()
+      .verifyIdToken(token)
+      .then((decodedToken) => {
+        /* 
+         Logic 2 goes here
+
+
+          */
+        next();
+      })
+      .catch(() => {
+        return res.send(respondWith(403, `Invalid Token`));
+      });
+  };
+};
+
+export { authToken };
