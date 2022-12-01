57facf438112acb4053aec2ce2ee6171e3027630
Paul Mikulskis <mikulskisp@gmail.com>
2022-11-30 21:44:50 -0500

    linting

--- src/workers/scanEntireChannel/index.ts
+++ src/workers/scanEntireChannel/index.ts
@@ -1,17 +1,15 @@
-import { createIntegratedWorker } from "../utils/worker";
-import ytpurge from "../../utils/ytpurgeapi";
+/* eslint-disable unused-imports/no-unused-vars */
+import { createIntegratedWorker } from "../utils/worker"
+import ytpurge from "../../utils/ytpurgeapi"
 
 export const scanEntireChannel = async () => {
-  await createIntegratedWorker(
-    "scanEntireChannel",
-    async ({ reqBody, _calls }) => {
-      const config = reqBody;
-      const response = await ytpurge.post(`/scan/userid`);
-      //Logic goes here
-      try {
-      } catch (e) {
-        console.log(`ERROR while trying to request for the api`);
-      }
+  await createIntegratedWorker("scanEntireChannel", async ({ reqBody, _calls }) => {
+    const config = reqBody
+    const response = await ytpurge.post(`/scan/userid`)
+    //Logic goes here
+    try {
+    } catch (e) {
+      console.log(`ERROR while trying to request for the api`)
     }
-  );
-};
+  })
+}
